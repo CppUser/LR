a482050f7c75fb770711dc82e1f7ec6fbffcf943
CppUser <ganscorp@gmail.com>
2025-09-24 12:09:58 -0400

    Plugin creation

---
+++ Plugins/QuestSystem/Source/QuestSystemEditor/Private/QuestSystemEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "QuestSystemEditor.h"
+
+#define LOCTEXT_NAMESPACE "FQuestSystemEditorModule"
+
+void FQuestSystemEditorModule::StartupModule()
+{
+    
+}
+
+void FQuestSystemEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FQuestSystemEditorModule, QuestSystemEditor)
